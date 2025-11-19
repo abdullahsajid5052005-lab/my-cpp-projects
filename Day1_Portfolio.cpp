ab236ef1a407801e0ec047e537c6133ce989cec7
abdullahsajid5052005-lab <abdullahsajid5052005@gmail.com>
2025-11-19 17:40:18 +0500

    Add cpp_practice_1.cpp with 5 C++ practice program
    This file include 5 biginner-level C++ programs written and saved as comments for practice and learning

---
+++ Day1_Portfolio.cpp
@@ -0,0 +1,67 @@
+//#include<iostream>
+//using namespace std;
+
+/*int main()
+{
+    cout<<"Hello World ";
+
+    return 0;
+}*/
+
+/*int main()
+{
+    int a = 6;
+    int b = 9;
+
+    cout<<"The sum is "<<a+b;
+
+    return 0;
+}*/
+
+/*int main()
+{
+    int a = 7;
+    int b = 12;
+
+    cout<<"The value of a = "<<a<<" the value of b = "<<b<<endl;
+    a = a+b;  // 7 = 7 + 12 it means a = 19
+    b = a-b;  // 12 = 19 - 12 means b = 7
+    a = a-b;  // 19 = 19 - 7 means a = 12
+
+    cout<<"The value of a = "<<a<<" and b = "<<b;
+
+    return 0;
+}*/
+
+/*int main()
+{
+    int a = 7;
+    int b = 8;
+    
+    int sum = a+b;
+    int sub = a-b;
+    int mul = a*b;
+    float div = a/b;
+
+    cout<<"sum is "<<sum<<endl;
+    cout<<"sub is "<<sub<<endl;
+    cout<<"mul is "<<mul<<endl;
+    cout<<"Div is "<<div<<endl;
+
+    return 0;
+}*/
+
+/*int main()
+{
+    int age = 17;
+
+    if(age >= 18)
+    {
+        cout<<"Adult";
+    }
+    else{
+        cout<<"Minor ";
+    }
+
+    return 0;
+}*/

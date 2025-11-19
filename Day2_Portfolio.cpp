dac35de1260dec764281f4fe140f3ad8c42a6ecc
abdullahsajid5052005-lab <abdullahsajid5052005@gmail.com>
2025-11-19 17:45:56 +0500

    Add cpp_practice_2.cpp with 5 C++ programs
    This is my Second set of C++ practice programs which are in commented

---
+++ Day2_Portfolio.cpp
@@ -0,0 +1,125 @@
+/*#include<iostream>
+using namespace std;*/
+
+/*int main()
+{
+   int a = 45;
+   int b = 76;
+   int c = 35;
+
+   if(a > b)
+   {
+      if(a>c)
+      {
+        cout<<"a is greater \n";
+      }
+
+      else{
+        cout<<"a is not greater \n";
+      }
+   }
+
+   else if(b > a)
+   {
+     if(b > c)
+     {
+        cout<<"b is greater \n";
+     }
+
+     else{
+        cout<<"b is not greater \n";
+     }
+   }
+
+   else{
+    cout<<"c is greater ";
+   }
+
+   return 0;
+}*/
+
+/*int main()
+{
+    int year = 2028;
+
+    if( (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+    {
+        cout<<"Year is heap "<<year<<endl;
+    }
+
+    else{
+        cout<<"year is not heap "<<endl;
+    }
+}*/
+
+/*int main()
+{
+    int number = 65;
+
+    char grade;
+
+    if(number >= 85 && number <=100)
+    {
+        grade = 'A';
+        cout<<"Grade is "<<grade;
+    }
+
+    else if(number >=65 && number < 85)
+    {
+        grade = 'B';
+        cout<<"Grade is "<<grade;
+    }
+
+    else if(number >=45 && number < 65)
+    {
+        grade = 'C';
+        cout<<"Grade is "<<grade;
+    }
+
+    else{
+        grade = 'F';
+        cout<<"Grade is "<<grade;
+    }
+
+    return 0;
+}*/
+
+/*int main()
+{
+    int number = 54;
+
+    if( number > 0)
+    
+    {
+        cout<<"Positive ";
+    }
+    else if(number < 0)
+    
+    {
+        cout<<"Negative"<<endl;
+    }
+
+    else{
+        cout<<"Zero"<<endl;
+    }
+
+    return 0;
+}*/
+
+/*int main()
+{
+    int num = 53;
+
+    if(num % 2 == 0)
+    {
+        cout<<"Even ";
+    }
+
+    else
+    {
+        cout<<"Odd ";
+    }
+
+    return 0;
+}*/
+

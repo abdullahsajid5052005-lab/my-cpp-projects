098cbc235784f8014917caa22c45b2e2c8daf948
abdullahsajid5052005-lab <abdullahsajid5052005@gmail.com>
2025-11-19 17:58:14 +0500

    Add cpp_practice_4.cpp with 5 C++ programs
    This is forth  C++ practice program in which  5 are in commented.

---
+++ Day4_Portfolio.cpp
@@ -0,0 +1,90 @@
+#include<iostream>
+using namespace std;
+
+/*int main()
+{
+   int N = 3;
+
+   for(int i=1 ; i<=N ; i++)
+   {
+      for(int j=1 ; j<=N ; j++)
+      {
+        cout<<"* ";
+      }
+
+      cout<<endl;
+   }
+
+    return 0;
+}*/
+
+/*int main()
+{
+    int N = 5;
+
+    for(int i=1;i<=N; i++)
+    {
+       for(int j= 1; j<= i; j++)
+       {
+          cout<<"* ";
+       }
+         cout<<endl;
+    }
+     
+    return 0;
+}*/
+
+/*int main()
+{
+    int N = 5;
+
+     for(int i=1;i<=N;i++)
+     {
+        for(int j=N;j>=i;j--)
+        {
+            cout<<"* ";
+        }
+         
+        //for(int j = i-1;j > 0;j--)
+        {
+            cout<<"* ";
+        }//
+        cout<<endl;
+     }
+
+      return 0;
+}*/
+
+/*int main()
+{
+    int N = 5;
+
+    for(int i=1;i<=N;i++)
+    {
+        for(int j=N; j>= i ; j--)
+        {
+            cout<<"* ";
+        }
+
+         cout<<endl;
+    }
+
+     return 0;
+}*/
+
+/*int main()
+{
+    int N = 5;
+    int n =0;
+    for(int i=1;i<=N;i++)
+    {
+       
+        for(int j = 1; j <= i ;j++)
+          {
+            n = n+1;
+            cout<<n<<" ";
+          }
+
+          cout<<endl;
+    }
+}*/
